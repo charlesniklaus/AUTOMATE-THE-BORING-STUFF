a10bbc1ba5eee728222f32ab405114233f9fa1fa
charlesniklaus <charlesniklaus@gmail.com>
2020-07-07 17:53:41 +0100

    Delete 01-Python-Basics.py

--- 01-Python-Basics.py
+++
@@ -1,8 +0,0 @@
-# Chapter 1 - Python Basics
-
-# Say Hello and ask for your name
-print('Hello world!')
-print('What is your name')
-myName = input()
-print('It is good to meet you, ' + myName)
-print('The length of your name is: ' + str(len(myName)))
